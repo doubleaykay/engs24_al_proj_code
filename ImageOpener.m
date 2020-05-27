6238bdf074c8394054427192b3f943bcc4257a28
doubleaykay <the.a.cars@gmail.com>
2020-05-26 21:05:38 -0400

    loop to continue when done with one image; code cleanup

--- ImageOpener.m
+++ ImageOpener.m
@@ -1,14 +1,29 @@
-%This script is for identifying the radii of holes in our ENGS 24 project
-[imgFileName,imgFilePath] = uigetfile('*.jpg');
-image = imread(strcat(imgFilePath,imgFileName));
-imshow(rgb2gray(image))
-distLine = imdistline;
-circleDia = distLine.getDistance
-[center, radius] = imfindcircles(rgb2gray(image),[120, 135],'ObjectPolarity', 'dark', 'Sensitivity', 0.99, 'EdgeThreshold',0.1)
-h = viscircles(center, radius);
+% This script is for identifying the radii of holes in our ENGS 24 project
+% Ben Martin, Anoush Khan
 
-%Need to add user radius guessing
-%Need to add a loop that allows the user to select a new image after the
-%first one is finished
-%Need to store the radius
-%Need to scale the radius to a real length based on a calibration factor
+% Need to add user radius guessing
+% Need to store the radius
+% Need to scale the radius to a real length based on a calibration factor
+
+run = true;
+while run = true
+    % execute code
+    % prompt user to select JPG image
+    [imgFileName,imgFilePath] = uigetfile('*.jpg');
+
+    % open selected image and start processing
+    image = imread(strcat(imgFilePath,imgFileName));
+    imshow(rgb2gray(image))
+    distLine = imdistline;
+    circleDia = distLine.getDistance
+    [center, radius] = imfindcircles(rgb2gray(image),[120, 135],'ObjectPolarity', 'dark', 'Sensitivity', 0.99, 'EdgeThreshold',0.1)
+    h = viscircles(center, radius);
+    
+    % ask if user wants to continue
+    inp = input('Do another image? y/n: ', 's');
+    if inp = 'y'
+        run = true;
+    elseif inp = 'n'
+        run = false;
+    end
+end
